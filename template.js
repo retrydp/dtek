41ecce839fab2ec5bb79a11cbc1c8e5367f278fe
retrydp <thetemon@gmail.com>
2026-02-16 09:41:04 +0200

    logo

--- template.js
+++ template.js
@@ -3,6 +3,9 @@ export const template = `<!doctype html><html lang="uk">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta property="og:image" content="https://www.dtek-dnem.com.ua/media/page/news-image-1200-1585899488629.png">
+    <meta itemprop="image" content="https://www.dtek-dnem.com.ua/media/page/news-image-1200-1585899488629.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
     <title>Графік</title>
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;600&display=swap');
