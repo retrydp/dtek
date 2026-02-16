94d35271c6d741d54b669e25a4602965a4282fe0
retrydp <thetemon@gmail.com>
2026-02-16 09:01:07 +0200

    view

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import Fastify from 'fastify';
-import evaluateData from './schedule.js';
+import Fastify from 'fastify';import evaluateData from './schedule.js';
+import fs from 'fs';
 const fastify = Fastify({
   logger: true,
 });
@@ -12,6 +12,24 @@ fastify.get('/', async (request, reply) => {
 fastify.get('/schedule', async (request, reply) => {
   const { today_schedule, tomorrow_schedule } = await evaluateData();
   reply.send({ today_schedule, tomorrow_schedule });
+});
+fastify.get('/page', async (request, reply) => {
+  const template = fs.readFileSync('./static/index.html', 'utf-8');
+  const stringifiedData = template.toString();
+  const { today_intervals, tomorrow_intervals } = await evaluateData();
+  const formatIntervals = (interval) => {
+    if (interval.length === 0) return 'Немає графіку на цей день';
+    const intervalStrings = interval.map(
+      ({ start, end }) => `<strong>Початок:</strong> ${start} - <strong>Кінець:</strong> ${end}`,
+    );
+
+    return intervalStrings.join('<br>');
+  };
+  const formatted = stringifiedData
+    .replace('{{today}}', formatIntervals(today_intervals))
+    .replace('{{tomorrow}}', formatIntervals(tomorrow_intervals));
+  reply.header('Content-Type', 'text/html');
+  reply.send(formatted);
 });
 
 fastify.listen({ port: 3000 }, function (err, address) {
